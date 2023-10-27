69f9676c32f8971c5e8945fde2b9836602075fbf
1006969989 <1006969989@qq.com>
2023-10-28 00:27:33 +0800

    仓储服务-仓库管理(合并、领取、处理采购单)

--- gulimall-ware/src/main/java/com/yuan/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/yuan/gulimall/ware/GulimallWareApplication.java
@@ -3,7 +3,11 @@ package com.yuan.gulimall.ware;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.yuan.gulimall.ware.feign")
+@EnableDiscoveryClient
 @MapperScan("com.yuan.gulimall.ware.dao")
 @SpringBootApplication
 public class GulimallWareApplication {
--- gulimall-ware/src/main/java/com/yuan/gulimall/ware/dao/WareSkuDao.java
+++ gulimall-ware/src/main/java/com/yuan/gulimall/ware/dao/WareSkuDao.java
@@ -3,15 +3,17 @@ package com.yuan.gulimall.ware.dao;
 import com.yuan.gulimall.ware.entity.WareSkuEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 商品库存
- * 
+ *
  * @author yuan
  * @email 1006969989@qq.com
  * @date 2023-10-17 16:09:30
  */
 @Mapper
 public interface WareSkuDao extends BaseMapper<WareSkuEntity> {
-	
+
+    void addStock(@Param("skuId") Long skuId,@Param("wareId") Long wareId,@Param("skuNum") Integer skuNum);
 }
