303907b6372f3558777b6d82e8fd1f5495ff34a8
1006969989 <1006969989@qq.com>
2023-11-06 07:41:59 +0800

    商城业务-商品详情页(异步编排)

--- gulimall-product/src/main/java/com/yuan/gulimall/product/dao/AttrGroupDao.java
+++ gulimall-product/src/main/java/com/yuan/gulimall/product/dao/AttrGroupDao.java
@@ -2,16 +2,21 @@ package com.yuan.gulimall.product.dao;
 
 import com.yuan.gulimall.product.entity.AttrGroupEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yuan.gulimall.product.vo.SpuItemAttrGroupVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 属性分组
- * 
+ *
  * @author yuan
  * @email 1006969989@qq.com
  * @date 2023-10-17 00:39:56
  */
 @Mapper
 public interface AttrGroupDao extends BaseMapper<AttrGroupEntity> {
-	
+
+    List<SpuItemAttrGroupVo> getAttrGroupWithAttrsBySpuId(@Param("spuId") Long spuId, @Param("catalogId") Long catalogId);
 }
--- gulimall-product/src/main/java/com/yuan/gulimall/product/dao/SkuSaleAttrValueDao.java
+++ gulimall-product/src/main/java/com/yuan/gulimall/product/dao/SkuSaleAttrValueDao.java
@@ -2,16 +2,21 @@ package com.yuan.gulimall.product.dao;
 
 import com.yuan.gulimall.product.entity.SkuSaleAttrValueEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yuan.gulimall.product.vo.SkuItemSaleAttrVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * sku销售属性&值
- * 
+ *
  * @author yuan
  * @email 1006969989@qq.com
  * @date 2023-10-17 00:39:56
  */
 @Mapper
 public interface SkuSaleAttrValueDao extends BaseMapper<SkuSaleAttrValueEntity> {
-	
+
+    List<SkuItemSaleAttrVo> getSaleAttrBySpuId(@Param("spuId") Long spuId);
 }
